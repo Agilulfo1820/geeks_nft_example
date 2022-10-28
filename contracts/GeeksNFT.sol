62232bacadc07c6ef54e80458c4592e6dc9870bd
Dan Rusnac <rusnac14@gmail.com>
2022-10-28 11:09:05 +0200

    Extended Openzeppelin ERC721 contract

--- contracts/GeeksNFT.sol
+++ contracts/GeeksNFT.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-contract GeeksNFT {
-    
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract GeeksNFT is ERC721 {
+    constructor() ERC721('GeeksNFT', 'GNFT'){
+
+    }
 }
